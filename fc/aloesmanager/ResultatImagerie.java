072f02eee51c227352bc0a1b5ad4759215ca1d84
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-03 13:12:17 +0100

    Update ResultatImagerie.java

--- fc/aloesmanager/ResultatImagerie.java
+++ fc/aloesmanager/ResultatImagerie.java
@@ -3,7 +3,16 @@ package fc.aloesmanager;
 import java.sql.*;
 
 public class ResultatImagerie extends Resultat {
-
-    private String compteRendu;
+    
+    /**
+     * Constructeur
+     */
+    public ResultatImagerie(String CR, Date date, String obs, String id, String nrpps){
+        this.compteRendu = CR;
+        this.date = date;
+        this.observations = obs;      
+        this.id = id;
+        this.nrpps = nrpps;
+    }
 
 }
