cf66e94cc39082c27027e1a30ffd57b3903a812b
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-03 14:54:32 +0100

    Update PrescriptionExamen.java

--- fc/aloesmanager/PrescriptionExamen.java
+++ fc/aloesmanager/PrescriptionExamen.java
@@ -45,52 +45,10 @@ public class PrescriptionExamen {
         //L'interface envoie des informations
         Connection con = ConnexionBDD.obtenirConnection();
         PreparedStatement creerPresExam = null;
-        PreparedStatement chercherNRPPS = null;
         ResultSet resultats_bd = null;
         java.util.Date utilDate = new java.util.Date();
         java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
-
-
-        //----------- Requête 1: recherche du n_rpps à partir de l'identifiant
-        try {
-            chercherNRPPS = con.prepareStatement("select * from correspondanceId natural join acces_serveur where identifiant_connexion =?");
-            chercherNRPPS.setString(1, identifiant);
-        } catch (Exception e) {
-            System.out.println("Erreur de requête 1");
-        }
-
-        //-----------Accès à la base de données
-        try {
-            resultats_bd = chercherNRPPS.executeQuery();
-        } catch (SQLException e) {
-            do {
-                System.out.println("Requête refusée");
-                System.out.println("SQLState : " + e.getSQLState());
-                System.out.println("Description : " + e.getMessage());
-                System.out.println("code erreur : " + e.getErrorCode());
-                System.out.println("");
-                e = e.getNextException();
-            } while (e != null);
-        }
-
-        //-----------parcours des données retournées
-        //---Variables temporaires
-        String n_rpps ="";
-        try {
-            while (resultats_bd.next()) {
-                n_rpps = resultats_bd.getString("n_rpps");          
-            }
-            resultats_bd.close();
-        } catch (SQLException e) {
-            do {
-                System.out.println("Accès aux résultats refusé");
-                System.out.println("SQLState : " + e.getSQLState());
-                System.out.println("Description : " + e.getMessage());
-                System.out.println("code erreur : " + e.getErrorCode());
-                System.out.println("");
-                e = e.getNextException();
-            } while (e != null);
-        }
+        String n_rpps = Connexion.returnNRPPS(identifiant);
         
         try {
             String requete = "INSERT INTO Prescription_examen VALUES(? , ? , ?, ?, ?, NULL)";
