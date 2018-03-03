dd1457a73f1f1c4d5507a5d12460ec08d6d7ea1e
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-03 14:29:25 +0100

    Update PrescriptionExamen.java

--- fc/aloesmanager/PrescriptionExamen.java
+++ fc/aloesmanager/PrescriptionExamen.java
@@ -43,35 +43,13 @@ public class PrescriptionExamen {
      */
     public void creerUnePrescriptionExamen(Examen examen, String id, String n_rpps, String exigences_examen) { //id du patient, identifiant de la personne connectée
         //L'interface envoie des informations
-        Connection con = null;
+        Connection con = ConnexionBDD.obtenirConnection();
         PreparedStatement creerPresExam = null;
         PreparedStatement chercherNRPPS = null;
         ResultSet resultats_bd = null;
         java.util.Date utilDate = new java.util.Date();
         java.sql.Date sqlDate = new java.sql.Date(utilDate.getTime());
 
-        //-----------Connexion
-        //Chargement du pilote
-        try {
-            Class.forName("com.mysql.jdbc.Driver"); //charge le pilote et crée une instance de cette classe
-        } catch (java.lang.ClassNotFoundException e) {
-            System.out.println("Erreur: Class Not Found");
-        }
-
-        //-----------Etablissement de la connexion
-        try {
-            String base1 = "azgardengineering_sih";
-            String DBurl1 = "jdbc:mysql://mysql-azgardengineering.alwaysdata.net/" + base1 + "?verifyServerCertificate=false&useSSL=true";
-            con = DriverManager.getConnection(DBurl1, "154118", "choco"); //remplacer le mot de passe
-        } catch (java.sql.SQLException e) {
-            do {
-                System.out.println("SQLState : " + e.getSQLState());
-                System.out.println("Description : " + e.getMessage());
-                System.out.println("code erreur : " + e.getErrorCode());
-                System.out.println("");
-                e = e.getNextException();
-            } while (e != null);
-        }
 
         //----------- Requête 1: recherche du n_rpps à partir de l'identifiant
         try {
