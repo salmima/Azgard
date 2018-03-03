696e40c8ea85e6a9a9daa1b957d1a64182e297e4
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-03 14:28:50 +0100

    Ajout chargement pilote & etablissement connexion

---
+++ fc/aloesmanager/ConnexionBDD.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fc.aloesmanager;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Thao
+ */
+public class ConnexionBDD {
+
+    public static Connection obtenirConnection() {
+
+        Connection con = null;
+
+        //-----------Connexion
+        //Chargement du pilote
+        try {
+            Class.forName("com.mysql.jdbc.Driver"); //charge le pilote et crée une instance de cette classe
+        } catch (java.lang.ClassNotFoundException e) {
+            System.out.println("Erreur: Class Not Found");
+        }
+
+        //-----------Etablissement de la connexion
+        try {
+            String base1 = "azgardengineering_sih";
+            String DBurl1 = "jdbc:mysql://mysql-azgardengineering.alwaysdata.net/" + base1 + "?verifyServerCertificate=false&useSSL=true";
+            con = DriverManager.getConnection(DBurl1, "154118", "choco"); //remplacer le mot de passe
+        } catch (java.sql.SQLException e) {
+            do {
+                System.out.println("SQLState : " + e.getSQLState());
+                System.out.println("Description : " + e.getMessage());
+                System.out.println("code erreur : " + e.getErrorCode());
+                System.out.println("");
+                e = e.getNextException();
+            } while (e != null);
+        }
+
+        return con;
+    }
+}
