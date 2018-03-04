62794686bb9d12d544c13261666cc56a044f61ab
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-04 11:38:45 +0100

    Update DossierMedical.java

--- fc/aloesmanager/DossierMedical.java
+++ fc/aloesmanager/DossierMedical.java
@@ -4,11 +4,6 @@ import java.util.ArrayList;
 import java.sql.*;
 
 public class DossierMedical {
-
-    private String groupeSanguin;
-
-    private Lit localisation;
-
     private ArrayList<DossierMedical> listeDMclinique;
 
 }
