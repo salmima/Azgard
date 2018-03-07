ea7d6125bfc7b54e1225ef503810c37daf9f6924
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-07 22:40:16 +0100

    ajout methode renvoie liste resultats

--- fc/aloesmanager/Resultat.java
+++ fc/aloesmanager/Resultat.java
@@ -46,5 +46,83 @@ public abstract class Resultat {
     public PH getPH(){
         return this.ph;
     }
+    
+    /**
+     * Méthode renvoie la liste des résultats pour un patient dans tous les
+     * services MT 
+     * Méthode à utiliser pour automatiser le remplissage de la
+     * lettre de sortie secteur est le secteur de la personne demandeur
+     */
+    public static ArrayList<Resultat> retourneListeResultat(String IPP, String secteur) {
+        Connection con = ConnexionBDD.obtenirConnection();
+        PreparedStatement rechercheRes = null;
+        ResultSet resultats_bd = null;
+        ArrayList<Resultat> listeRes = new ArrayList<Resultat>(); //on retourne une liste d'examens
+
+        //----------- Requête 1: recherche des résultats d'un certain service demandeur
+        try {
+            rechercheRes = con.prepareStatement("select * from Resultat where id = ? and lower(service_demandeur)=? ORDER BY date desc");
+            rechercheRes.setString(1, IPP);
+            rechercheRes.setString(2, secteur.toLowerCase());
+        } catch (Exception e) {
+            System.out.println("Erreur de requête 1");
+        }
+
+        //-----------parcours des données retournées
+        //---Variables temporaires
+        String r_compteRendu = "";
+        Date r_date = null;
+        String r_observations = "";
+        String r_nrpps = "";
+        String r_type_examen = "";
+        PH r_ph = new PH();
+        Resultat resultat = null;
+        try {
+            while (resultats_bd.next()) {
+                r_observations = resultats_bd.getString("observations");
+                r_compteRendu = resultats_bd.getString("compte_rendu");
+                r_date = resultats_bd.getDate("date");
+                r_nrpps = resultats_bd.getString("n_rpps");
+                r_type_examen = resultats_bd.getString("type_examen");
+
+                try {
+                    //On cherche le PH
+                    r_ph = r_ph.rechercherUnMedecinRPPS(r_nrpps);
+
+                    if (r_nrpps != null) {
+                        if (r_type_examen.equals("radiologie")) {
+                            resultat = new ResultatImagerie(r_compteRendu, r_date, r_observations, IPP, r_ph);
+                            listeRes.add(resultat);
+                        } else if (r_type_examen.equals("hematologie")) {
+                            resultat = new ResultatHematologie(r_compteRendu, r_date, r_observations, IPP, r_ph);
+                            listeRes.add(resultat);
+                        } else if (r_type_examen.equals("anatomopathologie")) {
+                            resultat = new ResultatAnapathologie(r_compteRendu, r_date, r_observations, IPP, r_ph);
+                            listeRes.add(resultat);
+                        } else if (r_type_examen.equals("biologie")) {
+                            resultat = new ResultatBiologique(r_compteRendu, r_date, r_observations, IPP, r_ph);
+                            listeRes.add(resultat);
+                        } //ajouter resultat anesthesie
+                        listeRes.add(resultat);
+                    }
+                } catch (Exception e) {
+                    System.out.println("");
+                }
+            }
+
+            resultats_bd.close();
+        } catch (SQLException e) {
+            do {
+                System.out.println("Accès aux résultats refusé 4");
+                System.out.println("SQLState : " + e.getSQLState());
+                System.out.println("Description : " + e.getMessage());
+                System.out.println("code erreur : " + e.getErrorCode());
+                System.out.println("");
+                e = e.getNextException();
+            } while (e != null);
+        }
+
+        return listeRes;
+    }
 
 }
