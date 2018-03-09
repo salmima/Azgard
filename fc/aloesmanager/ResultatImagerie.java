4e3a15bb73443be1b8a8277ae9f3cdc7a9d1e583
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-09 19:29:37 +0100

    Update ResultatImagerie.java

--- fc/aloesmanager/ResultatImagerie.java
+++ fc/aloesmanager/ResultatImagerie.java
@@ -8,12 +8,13 @@ public class ResultatImagerie extends Resultat {
     /**
      * Constructeur
      */
-    public ResultatImagerie(String CR, Date date, String obs, String id, PH ph){
+    public ResultatImagerie(String CR, Date date, String obs, String id, PH ph, PH ph_demandeur){
         this.compteRendu = CR;
         this.date = date;
         this.observations = obs;      
         this.id = id;
         this.ph = ph;
+        this.ph_demandeur = ph_demandeur;
     }
 
 }
