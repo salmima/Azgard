2f9cb9dca8693316a6e2dea836f689ac9cbfc857
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-09 19:29:52 +0100

    Update ResultatAnapathologie.java

--- fc/aloesmanager/ResultatAnapathologie.java
+++ fc/aloesmanager/ResultatAnapathologie.java
@@ -7,12 +7,13 @@ public class ResultatAnapathologie extends Resultat {
     /**
      * Constructeur
      */
-    public ResultatAnapathologie(String CR, Date date, String obs, String id, PH ph){
+    public ResultatAnapathologie(String CR, Date date, String obs, String id, PH ph, PH ph_demandeur){
         this.compteRendu = CR;
         this.date = date;
         this.observations = obs;      
         this.id = id;
         this.ph = ph;
+        this.ph_demandeur = ph_demandeur;
     }
 
 
