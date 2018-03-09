1efacaeb70db1e8a048fb57a93a364e9e6700572
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-09 19:29:45 +0100

    Update ResultatBiologique.java

--- fc/aloesmanager/ResultatBiologique.java
+++ fc/aloesmanager/ResultatBiologique.java
@@ -7,12 +7,13 @@ public class ResultatBiologique extends Resultat {
     /**
      * Constructeur
      */
-    public ResultatBiologique(String CR, Date date, String obs, String id, PH ph){
+    public ResultatBiologique(String CR, Date date, String obs, String id, PH ph, PH ph_demandeur){
         this.compteRendu = CR;
         this.date = date;
         this.observations = obs;      
         this.id = id;
         this.ph = ph;
+        this.ph_demandeur = ph_demandeur;
     }
 
 }
