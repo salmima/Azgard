0a69e284d3048e690fe2eaf80a07ed7403a31206
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-15 00:00:22 +0100

    ajout get

--- fc/aloesmanager/DMT.java
+++ fc/aloesmanager/DMT.java
@@ -17,4 +17,48 @@ public abstract class DMT {
     protected ArrayList<PrescriptionExamen> liste_examens;
     protected String observations;
     protected String id;
+    
+    /**
+     * Ajout d'un résultat à la liste d'examens
+     */
+    public void ajouterPrescription(PrescriptionExamen pres) {
+        this.liste_examens.add(pres);
+    }
+
+    /**
+     * Retourne les observations
+     */
+    public String getObs() {
+        return this.observations;
+    }
+
+    /**
+     * Retourne l'id
+     */
+    public String getId() {
+        return this.id;
+    }
+
+    /**
+     * Mise à jour deS observations
+     */
+    public void setObs(String obs) {
+        this.observations = obs;
+    }
+
+    /**
+     * Ajout d'un résultat à la liste de résultats
+     */
+    public void ajouterResultat(Resultat resultat) {
+        this.liste_resultats.add(resultat);
+    }
+    
+    
+    /**
+     * Récupération de la liste de résultats
+     */
+    public ArrayList<Resultat> getListeResultat() {
+        return this.liste_resultats;
+    }
+
 }
