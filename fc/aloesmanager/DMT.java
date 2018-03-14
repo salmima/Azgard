083ae33398294ecac73193d428a3000a80b9548d
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-14 23:52:06 +0100

    ajout modélisation

---
+++ fc/aloesmanager/DMT.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fc.aloesmanager;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Thao
+ */
+public abstract class DMT {
+
+    protected ArrayList<Resultat> liste_resultats;
+    protected ArrayList<PrescriptionExamen> liste_examens;
+    protected String observations;
+    protected String id;
+}
