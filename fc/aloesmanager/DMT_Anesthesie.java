fc84819651401f46b0ef2e635e6c18f0879eb790
Thao <36033478+Houlabalou@users.noreply.github.com>
2018-03-15 11:49:15 +0100

    UPDATE MAJEURE

--- fc/aloesmanager/DMT_Anesthesie.java
+++ fc/aloesmanager/DMT_Anesthesie.java
@@ -182,10 +182,8 @@ public class DMT_Anesthesie extends DMT {
                     r_date = resultats_bd2.getDate("date");
                     r_nrpps = resultats_bd2.getString("n_rpps");
                     r_service_demandeur = resultats_bd2.getString("service_demandeur");
-                }
-
-                resultats_bd2.close();
-                try {
+                    
+                    try {
                     //On cherche le PH
                     r_ph = r_ph.rechercherUnMedecinRPPS(r_nrpps);
 
@@ -197,6 +195,10 @@ public class DMT_Anesthesie extends DMT {
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
+                }
+
+                resultats_bd2.close();
+                
 
             } catch (SQLException e) {
                 do {
